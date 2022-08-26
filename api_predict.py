6e8d0f244246373d1ccffd7a6fda04a499e30ecd
Milton Gama Neto <mvgn@cin.ufpe.br>
2022-08-26 10:54:26 -0300

    Add basic file and endpoint step to deploy

---
+++ api_predict.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+ 
+app = FastAPI()
+ 
+model_path = 'model-prophet'
+loaded_model = None
+ 
+@app.post("/predict")
+async def predict(periods):
+    print(periods)
+    
+    global loaded_model
+    # Check if model is already loaded
+ 
+    # if not loaded_model:
+    #     loaded_model = tf.keras.models.load_model(model_path)
+ 
+    # Predict with the model
+    # prediction = loaded_model.predict_classes(image_data)
+    
+    return f'Predicted_Digit: {periods}'
